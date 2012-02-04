dd53c54f26e708f74a846d84916be37aee39f1ec
Aslam Najeebdeen <aslamnajeem@gmail.com>
2012-02-04 17:59:13 +0530

    Added permissions screen for admins

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Ticketee::Application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
 
   get 'awaiting_confirmation', :to => "users#confirmation", :as => 'confirm_user'
+  put '/admin/users/:user_id/permissions', :to => 'admin/permissions#update', :as => :update_user_permissions
 
   namespace :admin do
     root :to => "base#index"
-    resources :users
+    resources :users do
+      resources :permissions
+    end
   end
 
   root :to => "projects#index"
--- features/step_definitions/permissions_steps.rb
+++ features/step_definitions/permissions_steps.rb
@@ -11,6 +11,16 @@ Given permission_step do |user, permission, on, project|
   create_permission(user, find_project(project), permission)
 end
 
+When /^I check "([^"]*)" for "([^"]*)"$/ do |permission, name|
+  project    = Project.find_by_name!(name)
+  permission = permission.downcase.gsub(" ", "_")
+  field_id   = "permissions_#{project.id}_#{permission}"
+  step(%Q{When I check "#{field_id}"})
+end
+
+When /^When I check "([^"]*)"$/ do |checkbox|
+  check checkbox
+end
 
 def create_permission(email, object, action)
   Permission.create!(:user => find_user(email),
